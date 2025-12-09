193f2a8f4fdcabec4d11c7b0f04c19600f6e4257
QuaKinG911 <quakayn1@gmail.com>
2025-12-09 15:36:51 +0800

    save something

--- server/config/email.js
+++ server/config/email.js
@@ -1,5 +1,4 @@
 // For demo purposes, simulating email sending with console.log
-// In production, integrate with real email service like SendGrid, Mailgun, etc.
 
 export const sendEmail = async (to, subject, html) => {
   console.log(`[EMAIL SIMULATION] To: ${to}`);
